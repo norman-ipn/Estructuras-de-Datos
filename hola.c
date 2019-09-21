a2ef7f29b917d1f69677f4c1d41c94b0cee4e9b3
Rafael Norman Saucedo Delgado <rsaucedo@ipn.mx>
2019-09-20 19:37:31 -0500

    lista de alumnos arreglada

--- hola.c
+++ hola.c
@@ -1,33 +1,15 @@
-Norman
-//Gomez Cano Daniel Aaron Bolis man
+/*
+Gomez Cano Daniel Aaron 
 Flores Castro Luis Antonio
-monse
-//Arturo Briones Moline 2
-
-/*Mauricio Aviles Piña */
-//David Toledo Heredia
-
->>>>>>> 94c8b677dd7672aec3e77a80a10699fb9a92d0e2
-=======
-//jose bryan estrada bernal
->>>>>>> 3398eb489d9a0273502a96ef440a70b687118967
->>>>>>> e5cf5dfaf684fc053a5513de3dad53cbefc48919
-=======
-/* Rafael Norman Saucedo Delgado */
->>>>>>> 38f740b1fb74447a03ca16a49439ac628046e653
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 38f740b1fb74447a03ca16a49439ac628046e653
-=======
->>>>>>> 3398eb489d9a0273502a96ef440a70b687118967
->>>>>>> e5cf5dfaf684fc053a5513de3dad53cbefc48919
-<<<<<<< HEAD
-=======
+Arturo Briones Moline 
+Mauricio Aviles Piña 
+David Toledo Heredia
+José Bryan Estrada Bernal
+Rafael Norman Saucedo Delgado 
+Samuel barrios Avila 
+*/
 #include <stdlib.h>
->>>>>>> e8dcfdfe352f29fec0dcc0ab19c8f58be651e089
 #include <stdio.h>
-<<<<<<< HEAD
-//Samuel barrios Avila 
 =======
 //Ricardo Santiago Hernández
 >>>>>>> c2e61430d7b25e6c5e1f9a55aefab0a938594307
