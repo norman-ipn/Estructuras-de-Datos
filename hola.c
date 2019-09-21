4379b3ec85b7376820a584f4f14f00cc5c7acef8
Rafael Norman Saucedo Delgado <rsaucedo@ipn.mx>
2019-09-20 19:49:31 -0500

    ya esta ahora si, arreglado, no le muevan!

--- hola.c
+++ hola.c
@@ -7,36 +7,19 @@ David Toledo Heredia
 José Bryan Estrada Bernal
 Rafael Norman Saucedo Delgado 
 Samuel barrios Avila 
+Ricardo Santiago Hernández
+Mauricio Aviles Piña
+ivanna pamela avila reyna
+Gomez Cano Daniel Aaron 
+Ramírez Fuentes Edgar Alejandro
 */
 #include <stdlib.h>
 #include <stdio.h>
-=======
-//Ricardo Santiago Hernández
->>>>>>> c2e61430d7b25e6c5e1f9a55aefab0a938594307
 int
 main (void)
 {
-<<<<<<< HEAD
-  printf ("Hola mundo?\n");
-<<<<<<< HEAD
-	printf("Samuel Barrios A.");
-=======
->>>>>>> c2e61430d7b25e6c5e1f9a55aefab0a938594307
-  return 0;
-
-=======
-
   printf ("Hola mundo!!!\n");
->>>>>>> e8dcfdfe352f29fec0dcc0ab19c8f58be651e089
+  printf ("Samuel Barrios A.");
 
-=======
-/*Mauricio Aviles Piña*/
-ivanna pamela avila reyna
-<<<<<<< HEAD
->>>>>>> be1a2d674728b49932c33cc7e1c841b7a710aa5d
-=======
-Me pica una pompi
-//Gomez Cano Daniel Aaron Bolis man
->>>>>>> cba00a15e7e87c9baabe55ef685e73f4caf11637
-
-// Ramírez Fuentes Edgar Alejandro
+  return 0;
+}
