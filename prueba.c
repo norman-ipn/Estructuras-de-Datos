ddbe80e2bcd330f29c4d5f9e606eb1d0381c2c56
HugoAndres <huggo.ipn.1997@gmail.com>
2019-10-16 17:47:14 -0500

    Agregue nombre a la lista

--- prueba.c
+++ prueba.c
@@ -1,98 +1,15 @@
-<<<<<<< HEAD
-1CV11
+/*Grupo: 1CV11
+
+Integrantes:
+
 Guillermo Corona Muñoz
 David S Sotelo Ramos
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-Italuu Rojas Tapia 1CV11
-Guillermo Corona Muñoz
-1CV11
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-
-#include <stdio.h>
-
-int main(void)
-{
-
-
-	printf("Leon Tejeda \n");
-return 0;
-}	
-=======
-Guillermo Corona Muñoz
-1CV11
-hola que hace xD
-=======
-=======
->>>>>>> c18c3fe5131aeef86c1a00af485a86cc5b5c7a54
+Italuu Rojas Tapia 
+Leon Tejeda
 Ricardo Montoya Placido
-1cv11
-hola jajaja
-=======
-<<<<<<< HEAD
-=======
-
-
 Aguirre Cruz Israel Alejandro
-Grupo:1CV11
-Guillermo Corona Muñoz
-1CV11
-
-
-
-
->>>>>>> 616bcd78c09ea632043a0c636d7a6f288ffb94f8
->>>>>>> 5bac89b64ca1c2c21680b2628eb828fbccb0b515
+Hugo Andres Machorro Meléndez
 Mayte Pahua Rivera
-1CV11
-
->>>>>>> 5cef43a37628d093ffb04745f66ea5023db98158
-Guillermo Corona Muñoz
-1CV11
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-hola que hace x
-
-=======
-=======
-=======
-
-=======
->>>>>>> 9fc058496aae042ab835bb7e73fdc37204783225
-=======
-=======
-
->>>>>>> 187b39a14f9571c1bd58c073ffa1fca277b1fac4
->>>>>>> 5bac89b64ca1c2c21680b2628eb828fbccb0b515
-1CV11
 Jose Delgado 
-
-
-Guillermo Corona Muñoz
 Jakeline Deyanire Salgado Ramírez
-
->>>>>>> e29d8950ad12ad2ef0e2ca221994ae799230f06a
-
-<<<<<<< HEAD
->>>>>>> e29d8950ad12ad2ef0e2ca221994ae799230f06a
->>>>>>> 5cef43a37628d093ffb04745f66ea5023db98158
-=======
-
-
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> e29d8950ad12ad2ef0e2ca221994ae799230f06a
->>>>>>> 9fc058496aae042ab835bb7e73fdc37204783225
-=======
->>>>>>> 187b39a14f9571c1bd58c073ffa1fca277b1fac4
->>>>>>> 5bac89b64ca1c2c21680b2628eb828fbccb0b515
->>>>>>> 83d85cb0f5545d6c7159b8734e23cd298fbe0e0a
+*/
