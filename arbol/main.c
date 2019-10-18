de4fefab9440bf0ac02711cb1f7237c180d531ce
Rafael Norman Saucedo Delgado <rsaucedo@ipn.mx>
2019-10-18 15:44:01 -0500

    agregué insertar de arbol binario de búsqueda

---
+++ arbol/main.c
@@ -0,0 +1,50 @@
+#include<stdlib.h>
+struct Arbol
+{
+  int dato;
+  struct Arbol *izquierdo;
+  struct Arbol *derecho;
+};
+
+struct Arbol *
+insertar (struct Arbol *raiz, int dato)
+{
+  struct Arbol *nuevo = NULL;
+  /* caso base */
+  if (raiz == NULL)
+    {
+      nuevo = (struct Arbol *) malloc (sizeof (struct Arbol));
+      if (nuevo == NULL)
+	return NULL;
+      nuevo->dato = dato;
+      nuevo->izquierdo = NULL;
+      nuevo->derecho = NULL;
+      return nuevo;
+    }
+  if (dato == raiz->dato)
+    {
+      return raiz;
+    }
+  if (dato < raiz->dato)
+    {
+      raiz->izquierdo = insertar (raiz->izquierdo, dato);
+    }
+  else
+    {
+      raiz->derecho = insertar (raiz->derecho, dato);
+    }
+  return raiz;
+}
+
+int
+main (void)
+{
+  struct Arbol *raiz = NULL;
+  raiz = insertar (raiz, 10);
+  raiz = insertar (raiz, 1);
+  raiz = insertar (raiz, 140);
+  raiz = insertar (raiz, 510);
+  raiz = insertar (raiz, 160);
+  raiz = insertar (raiz, 180);
+  return 0;
+}
